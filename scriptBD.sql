8ee04982400a428561e37cd7c2d490f1eef44abe
Andrea Estrada Macias <andrea.estradam21@gmail.com>
2020-03-02 08:45:20 -0600

    Laboratorio 2 DS1 Avance

---
+++ scriptBD.sql
@@ -0,0 +1,20 @@
+CREATE TABLE video(
+idVideo int PRIMARY KEY,
+titulo varchar(100),
+repro int,
+url varchar(100)
+)
+
+CREATE PROCEDURE sp_video_insertar
+	@idVideo int,
+	@titulo varchar(100),
+	@repro int,
+	@url varchar(100)
+as
+begin
+	INSERT INTO video 
+	VALUES (@idVideo,@titulo,@repro,@url)
+end
+
+EXEC sp_video_insertar 1,'Video',1, 'x'
+SELECT * from video
